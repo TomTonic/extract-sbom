f704497689a0c3b0c3a521bf7dc40676e9bf1e2e
Tom Tonic <tom@tomtonic.de>
2026-04-10 22:45:42 +0200

    Fix linter issues

--- internal/safeguard/fuzz_test.go
+++ internal/safeguard/fuzz_test.go
@@ -49,12 +49,12 @@ func FuzzValidateEntry(f *testing.F) {
 	f.Add("file.txt", int64(1024), int64(512), false, false, uint32(0o644))
 	f.Add("bomb.bin", int64(1_000_000_000), int64(1), false, false, uint32(0o644))
 	f.Add("link", int64(0), int64(0), false, true, uint32(0o755))
-	f.Add("device", int64(0), int64(0), false, false, uint32(0o666|uint32(os.ModeDevice)))
+	f.Add("device", int64(0), int64(0), false, false, uint32(0o666|os.ModeDevice))
 	f.Add("dir/", int64(0), int64(0), true, false, uint32(0o755))
 	f.Add("", int64(-1), int64(-1), false, false, uint32(0))
-	f.Add("x", int64(^int64(0)>>1), int64(1), false, false, uint32(0o644)) // MaxInt64
+	f.Add("x", ^int64(0)>>1, int64(1), false, false, uint32(0o644)) // MaxInt64
 
-	f.Fuzz(func(t *testing.T, name string, uncompSize int64, compSize int64, isDir bool, isSymlink bool, rawMode uint32) {
+	f.Fuzz(func(_ *testing.T, name string, uncompSize int64, compSize int64, isDir bool, isSymlink bool, rawMode uint32) {
 		limits := config.DefaultLimits()
 		stats := &ExtractionStats{}
 		header := EntryHeader{
